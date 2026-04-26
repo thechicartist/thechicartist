1fd9c95e06f2e43e629f6b4b408650871b7b70d2
thechicartist <thechicartiststudio@gmail.com>
2026-04-25 22:34:39 -0400

    mark bookmarks and cards sold out

--- js/products.js
+++ js/products.js
@@ -38,7 +38,7 @@ const defaultBookmarkDesc = `
       type: 'physical',
       images: [`images/bookmarks/b${i}.jpeg`],
       description: defaultBookmarkDesc,
-      soldOut: false,
+      soldOut: true,
       category: 'bookmark'
     });
   }
@@ -86,7 +86,7 @@ const defaultCardDesc = `<p>An original hand-painted watercolor painting, crafte
       type: 'physical',
       images: [`images/cards/c${i}.jpeg`],
       description: defaultCardDesc,
-      soldOut: false,
+      soldOut: true,
       category: 'card'
     });
   }
